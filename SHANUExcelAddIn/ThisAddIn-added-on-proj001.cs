a7c082531bd998951473637904e3d63f79219951
zhanxuefeng <zhanxuefeng@LZHANXUEFENG>
2017-09-07 11:16:44 +0800

    update

--- SHANUExcelAddIn/ThisAddIn-added-on-proj001.cs
+++ SHANUExcelAddIn/ThisAddIn-added-on-proj001.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+***updated on proj001***
+
 using System.Xml.Linq;
 using Excel = Microsoft.Office.Interop.Excel;
 using Office = Microsoft.Office.Core;
